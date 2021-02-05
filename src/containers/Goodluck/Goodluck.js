0808896b819e4f78f2dda9898e606101c23cb073
Muhammad Yusuf Adi Purwanto <muhammadyusufadip.1999@gmail.com>
2021-02-05 09:57:40 +0700

    add ga and change modal in goodluck

--- src/containers/Goodluck/Goodluck.js
+++ src/containers/Goodluck/Goodluck.js
@@ -1,10 +1,21 @@
-import { ModalSuccess, Head } from "../../components";
+import { Head } from "../../components";
+import { icons } from "../../assets";
+import classes from "./Goodluck.module.scss";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
     <>
       <Head title="Goodluck | Elements" />
-      <ModalSuccess />
+      <div className={classes.success}>
+        <img src={icons.Success} alt="Download" className="w-52 h-52" />
+        <h1>Success Download!</h1>
+        <p>Gunakanlah design asset sebijaksana mungkin and Goodluck!</p>
+        <div>
+          <Link to="#">Our Team</Link>
+          <Link to="/">New Design</Link>
+        </div>
+      </div>
     </>
   );
 };
