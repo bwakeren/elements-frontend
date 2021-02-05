1afab495f3a127ce6c312fa47fb28105bb0244ff
Muhammad Yusuf Adi Purwanto <muhammadyusufadip.1999@gmail.com>
2021-02-05 21:27:05 +0700

    "adding hover animation"

--- src/components/Teams/TeamItem/TeamItem.js
+++ src/components/Teams/TeamItem/TeamItem.js
@@ -1,8 +1,29 @@
+import { useSpring, animated } from "react-spring";
+
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1,
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
 export const TeamItem = ({ name, job, illustration, socialMedia }) => {
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+
   return (
-    <div
+    <animated.div
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
       className="flex flex-col items-center w-72 bg-white py-8 rounded-3xl relative hover:shadow-product"
-      style={{ height: 340, width: 278 }}
+      style={{
+        height: 340,
+        width: 278,
+        transform: props.xys.interpolate(trans),
+      }}
     >
       {illustration}
       <h5 className="font-bold text-2xl mt-3 text-primary mb-2">{name}</h5>
@@ -20,6 +41,6 @@ export const TeamItem = ({ name, job, illustration, socialMedia }) => {
           </a>
         ))}
       </div>
-    </div>
+    </animated.div>
   );
 };
